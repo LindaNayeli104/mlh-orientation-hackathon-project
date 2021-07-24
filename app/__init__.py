01a89ffe3ca932266d3bbfad717406c865472787
LindaNayeli104 <linda_nayeli104@hotmail.com>
2021-07-23 22:03:02 -0500

    feat: add db connection to healthcheck endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -42,6 +42,13 @@ def index():
     return render_template("index.html", url=os.getenv("URL"))
 
 
+@app.route('/health')
+def health():
+    linda = UserModel.query.filter_by(username="linda").first()
+    has_linda = "yes" if linda is not None else "no"
+    return f"Works, has_linda: {has_linda}"
+
+
 @app.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
@@ -53,24 +60,21 @@ def register():
             error = "Username is required."
         elif not password:
             error = "Password is required."
-        elif (
-            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
-            is not None
-        ):
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                "INSERT INTO user (username, password) VALUES (?, ?)",
-                (username, generate_password_hash(password)),
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
 
+    return render_template("register.html", title="Register")
+
     """  T Return a restister page """
-    return "Register Page not yet implemented", 501
+    """ return "Register Page not yet implemented", 501 """
 
 
 @app.route("/login", methods=("GET", "POST"))
@@ -79,13 +83,11 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
         error = None
-        user = db.execute(
-            "SELECT * FROM user WHERE username = ?", (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = "Incorrect username."
-        elif not check_password_hash(user["password"], password):
+        elif not check_password_hash(user.password, password):
             error = "Incorrect password."
 
         if error is None:
@@ -93,5 +95,7 @@ def login():
         else:
             return error, 418
 
+    return render_template("login.html", title="Login")
+
     """ T Return a login page """
-    return "Login Page not yet implemented", 501
+    """ return "Login Page not yet implemented", 501 """
